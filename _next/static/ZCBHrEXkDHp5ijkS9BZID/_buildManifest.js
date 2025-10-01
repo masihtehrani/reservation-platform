f0db4f59419638f502b22eec198c620437cdcb17
masihtehrani <masihtehrani@users.noreply.github.com>
2025-10-01 11:09:54 +0000

    deploy: faa1ee87a54fc274dad3369823fe89d28ef9c8a2

---
+++ _next/static/ZCBHrEXkDHp5ijkS9BZID/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/b9eee36a8b4e734e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
